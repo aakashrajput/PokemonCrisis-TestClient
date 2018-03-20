3e98bf89211380f6bde8d91261370b105e4bcf44
Aakash <aakashsingh1999@gmail.com>
2018-03-21 02:01:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-
-	<script src="js/socket.js"></script>
-    <script src="js/nodeClient.js"></script>
-    <script src="js/client-topbar.js"></script>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script src="js/socket.js"></script>
+<script src="js/socket.io.slim.js"></script>
+<script src="js/nodeClient.js"></script>
+<script src="js/client-topbar.js"></script>
+<script src="js/userlogin.js"></script>
+<link rel="stylesheet" type="text/css" href="style/choosename.css">
